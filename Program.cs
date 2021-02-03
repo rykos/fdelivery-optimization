723709c00753df6e991bb8ae46f027e2a26508c4
Michał Rykowski <rykos9999@gmail.com>
2021-02-03 12:51:22 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.ServiceProcess;
+
+namespace fdelivery_optimization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceController[] scServices;
+            scServices = ServiceController.GetServices();
+            ServiceController deliveryOptimization = scServices.FirstOrDefault(s => s.DisplayName == "Delivery Optimization");
+            if (deliveryOptimization == default)
+            {
+                Console.WriteLine("'Delivery Optimization' service could not be found");
+                Console.ReadKey();
+            }
+            else
+            {
+                StopService(deliveryOptimization);
+            }
+        }
+
+        static void StopService(ServiceController service)
+        {
+            Console.WriteLine("'Delivery Optimization' stopper activated");
+            while (true)
+            {
+                if (service.Status == ServiceControllerStatus.Running)
+                {
+                    service.Stop();
+                    service.WaitForStatus(ServiceControllerStatus.Stopped);
+                    Console.WriteLine("update stopped");
+                }
+                service.WaitForStatus(ServiceControllerStatus.Running);
+            }
+        }
+    }
+}
